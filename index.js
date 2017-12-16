df231108f0c06903fe1a4b1db58951ca2b5e2707
Louis Mazza <louismazza@gmail.com>
2017-12-16 08:38:55 -0800

    testing 2nd ajax

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const UserCollection = [];
+let UserCollection = [];
 
 const TempData = '<items totalitems="4" termsofuse="http://boardgamegeek.com/xmlapi/termsofuse" pubdate="Wed, 13 Dec 2017 19:31:43 +0000"><item objecttype="thing" objectid="31260" subtype="boardgame" collid="47571802"><name sortindex="1">Agricola</name><yearpublished>2007</yearpublished><stats minplayers="1" maxplayers="5" minplaytime="30" maxplaytime="150" playingtime="150" numowned="61658"><rating value="N/A"><usersrated value="51092"/><average value="8.03861"/><bayesaverage value="7.94123"/><stddev value="1.56252"/><median value="0"/><ranks><rank type="subtype" id="1" name="boardgame" friendlyname="Board Game Rank" value="15" bayesaverage="7.94123"/><rank type="family" id="5497" name="strategygames" friendlyname="Strategy Game Rank" value="15" bayesaverage="7.92355"/></ranks></rating></stats>  <status own="1" prevowned="0" fortrade="0" want="0" wanttoplay="0" wanttobuy="0" wishlist="0" preordered="0" lastmodified="2017-12-11 00:33:21"/><numplays>0</numplays></item><item objecttype="thing" objectid="178900" subtype="boardgame" collid="47571809"><name sortindex="1">Codenames</name><yearpublished>2015</yearpublished><stats minplayers="2" maxplayers="8" minplaytime="15" maxplaytime="15" playingtime="15" numowned="57721"><rating value="N/A"><usersrated value="36542"/><average value="7.83777"/><bayesaverage value="7.74049"/><stddev value="1.25618"/><median value="0"/><ranks><rank type="subtype" id="1" name="boardgame" friendlyname="Board Game Rank" value="35" bayesaverage="7.74049"/><rank type="family" id="5498" name="partygames" friendlyname="Party Game Rank" value="1" bayesaverage="7.77027"/></ranks></rating></stats><status own="1" prevowned="0" fortrade="0" want="0" wanttoplay="0" wanttobuy="0" wishlist="0" preordered="0" lastmodified="2017-12-11 00:33:47"/><numplays>0</numplays></item></items>'
 
@@ -40,7 +40,7 @@ function addMoreGameInfo(data) {
 
 
 
-function findMoreGameInfo (element) {
+function getMoreGameInfo (element) {
   //given a gameId, this function searches for that "thing" and returns its 
   //description and player poll results and appends it to object
   $.ajax({
@@ -107,6 +107,7 @@ function handleResults (data) {
 function watchSubmit () {
   $('#query-form').submit(event => {
     event.preventDefault();
+    UserCollection = [];
     $.ajax({
       url: 'https://www.boardgamegeek.com/xmlapi2/collection',
       type: "GET",
@@ -127,7 +128,10 @@ function watchSubmit () {
     }).then(function(){
       let filteredCollection = filterByCollectionParameters(UserCollection,$('#playtime').val(),$('#player-number').val());
       console.log(filteredCollection);
-      })
+      let displayCollection = filteredCollection.map(getMoreGameInfo);
+      console.log(displayCollection);
+  
+        })
   });
 }
 
